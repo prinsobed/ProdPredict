f57f9be3ed2d429385a81ef1c6af95f86204ffab
Obed Kraine <prinsobed@gmail.com>
2016-08-05 09:21:40 +0100

    Query Pages Redo

--- main/forecast/forecast_check_query.php
+++ main/forecast/forecast_check_query.php
@@ -32,6 +32,7 @@ if ($conn->connect_error) {
     <script src="../../assets/js/bootstrap.min.js"></script>
     <script src="../../assets/js/bootstrap.js"></script>
 	<script src="../../assets/js/npm.js"></script>
+    <script src="../../assets/js/npm.js"></script>
 </head>
 <!-- End of Head -->
 
