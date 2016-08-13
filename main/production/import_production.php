c4295171687895ae9f760dcebd6fac781595f6d1
Obed Kraine <prinsobed@gmail.com>
2016-08-13 23:30:45 +0100

    Upload

--- main/production/import_production.php
+++ main/production/import_production.php
@@ -121,50 +121,50 @@ session_start();
             <!-- Main Section of Page for Analysis Option Selection, Showing or Editing Data/Graph -->
             <section>
 
-                <?php
-                $target_dir = "uploads/";
-                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-                $uploadOk = 1;
-                $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
-                // Check if file is a actual real orfake
-                if(isset($_POST["submit"])) {
-                    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
-                    if($check !== false) {
-                        echo "File is real - " . $check["mime"] . ".";
-                        $uploadOk = 1;
-                    } else {
-                        echo "File is real.";
-                        $uploadOk = 0;
-                    }
-                }
-                // Check if file already exists
-                if (file_exists($target_file)) {
-                    echo "Sorry, file already exists.";
-                    $uploadOk = 0;
-                }
-                // Check file size
-                if ($_FILES["fileToUpload"]["size"] > 500000) {
-                    echo "Sorry, your file is too large.";
-                    $uploadOk = 0;
-                }
-                // Allow certain file formats
-                if($fileType != "csv" && $fileType != "xls" && $fileType != "xlsx") {
-                    echo "Sorry, only CSV and XLS/XLSX files are allowed.";
-                    $uploadOk = 0;
-                }
-                // Check if $uploadOk is set to 0 by an error
-                if ($uploadOk == 0) {
-                    echo "Sorry, your file was not uploaded.";
-                // if everything is ok, try to upload file
-                } else {
-                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                    } else {
-                        echo "Sorry, there was an error uploading your file.";
-                    }
-                }
-                ?>
+<!--                --><?php
+//                $target_dir = "uploads/";
+//                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//                $uploadOk = 1;
+//                $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+//
+//                // Check if file is a actual real orfake
+//                if(isset($_POST["submit"])) {
+//                    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+//                    if($check !== false) {
+//                        echo "File is real - " . $check["mime"] . ".";
+//                        $uploadOk = 1;
+//                    } else {
+//                        echo "File is real.";
+//                        $uploadOk = 0;
+//                    }
+//                }
+//                // Check if file already exists
+//                if (file_exists($target_file)) {
+//                    echo "Sorry, file already exists.";
+//                    $uploadOk = 0;
+//                }
+//                // Check file size
+//                if ($_FILES["fileToUpload"]["size"] > 500000) {
+//                    echo "Sorry, your file is too large.";
+//                    $uploadOk = 0;
+//                }
+//                // Allow certain file formats
+//                if($fileType != "csv" && $fileType != "xls" && $fileType != "xlsx") {
+//                    echo "Sorry, only CSV and XLS/XLSX files are allowed.";
+//                    $uploadOk = 0;
+//                }
+//                // Check if $uploadOk is set to 0 by an error
+//                if ($uploadOk == 0) {
+//                    echo "Sorry, your file was not uploaded.";
+//                // if everything is ok, try to upload file
+//                } else {
+//                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+//                    } else {
+//                        echo "Sorry, there was an error uploading your file.";
+//                    }
+//                }
+//                ?>
 
 
 
@@ -178,7 +178,7 @@ session_start();
   							<article>
                                 <div id="main_feature">
 
-                                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+                                    <form action="upload.php" method="post" enctype="multipart/form-data">
                                         Select image to upload:
                                         <input type="file" name="fileToUpload" id="fileToUpload">
                                         <input type="submit" value="Upload File" name="submit">
