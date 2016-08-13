29d2c4103b3e574591dfc4872424ed1f7c255b25
Obed Kraine <prinsobed@gmail.com>
2016-08-13 22:53:11 +0100

    Upload

--- main/production/import_production.php
+++ main/production/import_production.php
@@ -121,74 +121,54 @@ session_start();
             <!-- Main Section of Page for Analysis Option Selection, Showing or Editing Data/Graph -->
             <section>
 
-<!--<!--                -->--><?php
-////                $target_dir = "../main/uploads/";
-////                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-////                $uploadOk = 1;
-////                $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-////                // Check if image file is a actual image or fake image
-////                if(isset($_POST["submit"])) {
-////                    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-////                    if($check !== false) {
-////                        echo "File is an image - " . $check["mime"] . ".";
-////                        $uploadOk = 1;
-////                    } else {
-////                        echo "File is not an image.";
-////                        $uploadOk = 0;
-////                    }
-////                }
-////                // Check if file already exists
-////                if (file_exists($target_file)) {
-////                    echo "Sorry, file already exists.";
-////                    $uploadOk = 0;
-////                }
-////                // Check file size
-////                if ($_FILES["fileToUpload"]["size"] > 500000) {
-////                    echo "Sorry, your file is too large.";
-////                    $uploadOk = 0;
-////                }
-////                // Allow certain file formats
-////                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-////                    && $imageFileType != "gif" ) {
-////                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-////                    $uploadOk = 0;
-////                }
-////                // Check if $uploadOk is set to 0 by an error
-////                if ($uploadOk == 0) {
-////                    echo "Sorry, your file was not uploaded.";
-////                // if everything is ok, try to upload file
-////                } else {
-////                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-////                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-////                    } else {
-////                        echo "Sorry, there was an error uploading your file.";
-////                    }
-////                }
-////                ?>
-<!---->
-<!---->
-<!--                --><?php
-//                if(isset($_POST['btn-upload']))
-//                {
-//                    $pic = rand(1000,100000)."-".$_FILES['pic']['name'];
-//                    $pic_loc = $_FILES['pic']['tmp_name'];
-//                    $folder="uploads/";
-//                    if(move_uploaded_file($pic_loc,$folder.$pic))
-//                    {
-//                        ?><!--<script>alert('Successfully uploaded');</script>--><?php
-//                    }
-//                    else
-//                    {
-//                        ?><!--<script>alert('Error while uploading file');</script>--><?php
-//                    }
-//                }
-//
-//                ?>
+                <?php
+                $target_dir = "uploads/";
+                $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+                $uploadOk = 1;
+                $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+                // Check if file is a actual real orfake
+                if(isset($_POST["submit"])) {
+                    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+                    if($check !== false) {
+                        echo "File is real - " . $check["mime"] . ".";
+                        $uploadOk = 1;
+                    } else {
+                        echo "File is real.";
+                        $uploadOk = 0;
+                    }
+                }
+                // Check if file already exists
+                if (file_exists($target_file)) {
+                    echo "Sorry, file already exists.";
+                    $uploadOk = 0;
+                }
+                // Check file size
+                if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                }
+                // Allow certain file formats
+                if($fileType != "csv" && $fileType != "xls" && $fileType != "xlsx") {
+                    echo "Sorry, only CSV and XLS/XLSX files are allowed.";
+                    $uploadOk = 0;
+                }
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded.";
+                // if everything is ok, try to upload file
+                } else {
+                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                    } else {
+                        echo "Sorry, there was an error uploading your file.";
+                    }
+                }
+                ?>
 
 
 
-
-            <div class="col-sm-9">
+                <div class="col-sm-9">
             	
   					<div class="panel panel-default">
     				<div class="panel-heading">Import New Production</div>
@@ -196,53 +176,24 @@ session_start();
                     	<div class="row2">
                         <!-- History -->
   							<article>
-        <div id="main_feature">
-<!--            <form action="--><?php //echo htmlspecialchars($_SERVER["PHP_SELF"]);?><!--" method="post" enctype="multipart/form-data">-->
-<!--                Select image to upload:-->
-<!--                <input type="file" name="fileToUpload" id="fileToUpload">-->
-<!--                <input type="submit" value="Upload Image" name="submit">-->
-<!--            </form>-->
-            
-<!--            <form id="upload" action="--><?php //echo htmlspecialchars($_SERVER["PHP_SELF"]);?><!--" method="POST" enctype="multipart/form-data">-->
-<!--            <fieldset>-->
-<!--                <input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />-->
-<!--                <div>-->
-<!--	                <label for="fileselect">Files to upload:</label>-->
-<!--	                <input type="file" id="fileToUpload" name="fileToUpload" />-->
-<!--	            <div id="filedrag">or drop files here</div>-->
-<!--                </div>-->
-<!---->
-<!--                <div>-->
-<!--                    <input type="reset" value="Clear">-->
-<!--                    <input type="submit" value="Upload File">-->
-<!--                </div>-->
-<!--            </fieldset>-->
-<!--            </form>-->
+                                <div id="main_feature">
 
-<!--            <form action="" method="post" enctype="multipart/form-data">-->
-<!--                <input type="file" name="pic" />-->
-<!--                <button type="submit" name="btn-upload">upload</button>-->
-<!--            </form>-->
-
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-                Select image to upload:
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload Image" name="submit">
-            </form>
+                                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+                                        Select image to upload:
+                                        <input type="file" name="fileToUpload" id="fileToUpload">
+                                        <input type="submit" value="Upload Image" name="submit">
+                                    </form>
 
 
-            <div id="messages">
-<p>Status Messages</p>
-</div>
-            
-        </div>
-    </article>
+                                </div>
+                            </article>
                             
                             
                   
+                        </div>
+			        </div>
+                    </div>
             </div>
-			</div>
-            </div></div>
     		</section>
             
     </main>
