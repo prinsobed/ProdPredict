173b3395aac5bce03a562502cf997801b1275b3a
Obed Kraine <prinsobed@gmail.com>
2016-08-16 23:54:27 +0100

    try insert- random data

--- main/production/import_production_ii.php
+++ main/production/import_production_ii.php
@@ -167,19 +167,18 @@ if ($conn->connect_error) {
                                         echo '$data[0]','$data[1]',$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10];
 
 }
-//                                    $import="INSERT into production(production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]','$data[1]',$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],11)";
-//
-//                                    mysqli_query($conn, $import) or die(mysqli_error($conn));
-//                                    }
-//
-//                                    fclose($handle);
-//
-//                                    print "Import done";
-//
-//                                    //view upload form
-//                                    }else {
-//
-//                                    print "Upload new csv by browsing to file and clicking on Upload<br />\n";
+                                    $import="INSERT into production(production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]','$data[1]',$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],11)";
+
+                                    mysqli_query($conn, $import) or die(mysqli_error($conn));
+
+                                    fclose($handle);
+
+                                    print "Import done";
+
+                                    //view upload form
+                                    }else {
+
+                                    print "Upload new csv by browsing to file and clicking on Upload<br />\n";
 
 //                                    print "<form enctype='multipart/form-data' action='upload.php' method='post'>";
 //
