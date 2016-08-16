6709b2366dcda2c521efafccb5038e75c3603af0
Obed Kraine <prinsobed@gmail.com>
2016-08-16 23:35:25 +0100

    try insert- random data

--- main/production/import_production_ii.php
+++ main/production/import_production_ii.php
@@ -163,7 +163,7 @@ if ($conn->connect_error) {
                                         //insert
 
                                     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                                    $import="INSERT into production(production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]','$data[1]',$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],{$_SESSION['id']})";
+                                    $import="INSERT into production(production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]','$data[1]',$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],11)";
 
                                     mysqli_query($conn, $import) or die(mysqli_error($conn));
                                     }
