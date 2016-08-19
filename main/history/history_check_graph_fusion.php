6379a42e7d9a991029a3e1cc03a735a8fad33deb
Obed Kraine <prinsobed@gmail.com>
2016-08-19 07:57:22 +0100

    Fusion Try Again and Again

--- main/history/history_check_graph_fusion.php
+++ main/history/history_check_graph_fusion.php
@@ -192,7 +192,7 @@ if ($dbhandle->connect_error) {
 }
 
 // Form the SQL query that returns the top 10 most populous countries
-$strQuery = "SELECT well, oil FROM production ORDER BY oil DESC";
+$strQuery = "SELECT city, population FROM cities ORDER BY population DESC LIMIT 10";
 
 // Execute the query, or else return the error message.
 $result = $dbhandle->query($strQuery) or exit("Error code ({$dbhandle->errno}): {$dbhandle->error}");
@@ -202,7 +202,7 @@ if ($result) {
     // The `$arrData` array holds the chart attributes and data
     $arrData = array(
         "chart" => array(
-            "caption" => "Wells and Oils",
+            "caption" => "Cities and Pops.",
             "paletteColors" => "#0075c2",
             "bgColor" => "#ffffff",
             "borderAlpha"=> "20",
@@ -225,8 +225,8 @@ if ($result) {
     // Push the data into the array
     while($row = mysqli_fetch_array($result)) {
         array_push($arrData["data"], array(
-                "label" => $row["Well"],
-                "value" => $row["Oil"]
+                "label" => $row["City"],
+                "value" => $row["Pop"]
             )
         );
     }
@@ -235,7 +235,11 @@ if ($result) {
 
     $jsonEncodedData = json_encode($arrData);
 
-    /*Create an object for the column chart using the FusionCharts PHP class constructor. Syntax for the constructor is ` FusionCharts("type of chart", "unique chart id", width of the chart, height of the chart, "div id to render the chart", "data format", "data source")`. Because we are using JSON data to render the chart, the data format will be `json`. The variable `$jsonEncodeData` holds all the JSON data for the chart, and will be passed as the value for the data source parameter of the constructor.*/
+    /*Create an object for the column chart using the FusionCharts PHP class constructor.
+    Syntax for the constructor is ` FusionCharts("type of chart", "unique chart id", width of the chart, height of the chart,
+    "div id to render the chart", "data format", "data source")`. Because we are using JSON data to render the chart,
+    the data format will be `json`. The variable `$jsonEncodeData` holds all the JSON data for the chart,
+    and will be passed as the value for the data source parameter of the constructor.*/
 
     $columnChart = new FusionCharts("column2D", "myFirstChart" , 600, 300, "chart", "json", $jsonEncodedData);
 
