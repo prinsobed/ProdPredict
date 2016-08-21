f28777ddd2e7cbeff3a4ec479dfd56233afb6dc4
Obed Kraine <prinsobed@gmail.com>
2016-08-21 02:40:14 +0100

    WELL selected and date selected + Update

--- main/production/p_includes/update_p.php
+++ main/production/p_includes/update_p.php
@@ -21,20 +21,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 /**********************************************/
-$prod_well = test_input($_POST["prod_well"]);
-$prod_date = test_input($_POST["prod_date"]);
-$oil_prod = test_input($_POST["oil_prod"]);
-$gas_prod = test_input($_POST["gas_prod"]);
-$water_prod = test_input($_POST["water_prod"]);
-$gas_oil_ratio = test_input($_POST["gas_oil_ratio"]);
-$basic_sed_water = test_input($_POST["basic_sed_water"]);
-$bean = test_input($_POST["bean"]);
-$tubing_hang_press = test_input($_POST["tubing_hang_press"]);
-$bottom_hole_pressure = test_input($_POST["bottom_hole_pressure"]);
-$a_p_i= test_input($_POST["a_p_i"]);
-$ent_user = test_input($_SESSION['id']);
 
-$sql = "UPDATE production SET oil='oil_prod', gas='gas_prod', water='water_prod', gor='gas_oil_ratio', bsw='basic_sed_water' bean='bean', thp='tubing_hang_press', bhp='bottom_hole_pressure' WHERE well = $wellSelect AND production_date = $dateSelect";
+$sql = "UPDATE production SET oil='oil_prod', gas='gas_prod', water='water_prod', gor='gas_oil_ratio', bsw='basic_sed_water' bean='bean', thp='tubing_hang_press', bhp='bottom_hole_pressure' WHERE well = 'prod_well' AND production_date = 'prod_date'";
 $result=mysqli_query($conn, $sql) or die ("");
 
 
