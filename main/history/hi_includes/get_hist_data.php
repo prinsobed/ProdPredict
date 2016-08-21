e076cfaa6f93ad2f02e2c30aaa68794fb816aa0e
Obed Kraine <prinsobed@gmail.com>
2016-08-21 20:33:08 +0100

    History

--- main/history/hi_includes/get_hist_data.php
+++ main/history/hi_includes/get_hist_data.php
@@ -35,7 +35,7 @@ $h_hydro = $_POST['hydrocarbon'];
 $rep_type = $_POST['report_type'];
 
 // Form the SQL query that returns the top 10 most populous countries
-$strQuery = "SELECT * FROM production WHERE well = $h_well AND production_date BETWEEN $h_start AND $h_end ORDER BY production_date ASC";
+$strQuery = "SELECT * FROM production WHERE well = $h_well AND production_date BETWEEN #$h_start# AND #$h_end# ORDER BY production_date ASC";
 
 $result = mysqli_query($conn, $strQuery);
 while($row = mysqli_fetch_array($result)) {
