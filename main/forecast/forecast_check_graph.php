c7b947a849e82a97930711d7ce4377fb26bf5696
Obed Kraine <prinsobed@gmail.com>
2016-08-21 11:18:20 +0100

    Forecast Issues + Graph Footer

--- main/forecast/forecast_check_graph.php
+++ main/forecast/forecast_check_graph.php
@@ -150,7 +150,9 @@ if(!$_SESSION['username']){
                         </div>
                     </div>
                     <div class="panel-footer">
-                        <a class="btn btn-default" href="" role="button">Export to File</a>
+                        <a class="btn btn-default" href="" role="button">Analysis Results</a>&nbsp;
+                        <a class="btn btn-default" href="" role="button">Print Graph</a>&nbsp;
+                        <a class="btn btn-default" href="" role="button">Export Graph to File</a>
                     </div>
                 </div></div>
         </section>
