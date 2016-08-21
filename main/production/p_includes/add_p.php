5b21f6bfacb9a47b3e874e20defe81abffd92af6
Obed Kraine <prinsobed@gmail.com>
2016-08-21 12:35:11 +0100

    Add Production Fix 2

--- main/production/p_includes/add_p.php
+++ main/production/p_includes/add_p.php
@@ -21,7 +21,7 @@ if ($conn->connect_error) {
 }
 
 // define variables and set to empty values
-$prod_well = $prod_date = $oil_prod = $gas_prod = $water_prod = $gas_oil_ratio = $basic_sed_water = $bean = $tubing_hang_press = $bottom_hole_pressure = $a_p_i = $ent_user = "";
+//$prod_well = $prod_date = $oil_prod = $gas_prod = $water_prod = $gas_oil_ratio = $basic_sed_water = $bean = $tubing_hang_press = $bottom_hole_pressure = $a_p_i = $ent_user = "";
 //
 //if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //    $prod_well = test_input($_POST["prod_well"]);
@@ -36,7 +36,21 @@ $prod_well = $prod_date = $oil_prod = $gas_prod = $water_prod = $gas_oil_ratio =
 //    $bottom_hole_pressure = test_input($_POST["bottom_hole_pressure"]);
 //    $a_p_i= test_input($_POST["a_p_i"]);
 //    $ent_user = test_input($_SESSION['id']);
-//}
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $prod_well = $_POST['prod_well'];
+        $prod_date = $_POST['prod_date'];
+        $oil = $_POST['oil_prod'];
+        $gas = $_POST['gas_prod'];
+        $water = $_POST['water_prod'];
+        $gor = $_POST['gas_oil_ratio'];
+        $bsw = $_POST['basic_sed_water'];
+        $bean = $_POST['bean'];
+        $thp = $_POST['tubing_hang_press'];
+        $bhp = $_POST['bottom_hole_pressure'];
+        $a_p_i= $_POST["a_p_i"];
+        $ent_user = $_SESSION['id'];
+}
 //
 //function test_input($data) {
 //    $data = trim($data);
@@ -44,51 +58,21 @@ $prod_well = $prod_date = $oil_prod = $gas_prod = $water_prod = $gas_oil_ratio =
 //    $data = htmlspecialchars($data);
 //    return $data;
 //}
-//
-//$sql = "INSERT INTO production(well, production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user)
-//                          VALUES ('$prod_well','$prod_date','$oil_prod','$gas_prod', '$water_prod', '$gas_oil_ratio','$basic_sed_water','$bean', '$tubing_hang_press','$bottom_hole_pressure','$a_p_i','$ent_user')";
-//
-//if ($conn->query($sql) === TRUE) {
-//    header("location: ../view_production.php");
-//}
-//else{
-//    header("location: ../add_production.php");
-//}
-//
-//$conn->close();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $prod_well = $_POST["prod_well"];
-    $prod_date = $_POST["prod_date"];
-    $oil = $_POST['oil_prod'];
-    $gas = $_POST['gas_prod'];
-    $water = $_POST['water_prod'];
-    $gor = $_POST['gas_oil_ratio'];
-    $bsw = $_POST['basic_sed_water'];
-    $bean = $_POST['bean'];
-    $thp = $_POST['tubing_hang_press'];
-    $bhp = $_POST['bottom_hole_pressure'];
-    $well = $_POST['prod_well'];
-    $production_date = $_POST['prod_date'];
-    $ent_user = $_SESSION['id'];
+$sql = "INSERT INTO production(well, production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user)
+                          VALUES ('$prod_well','$prod_date','$oil','$gas', '$water', '$gor','$bsw','$bean', '$thp','$bhp','$a_p_i','$ent_user')";
 
-//    INSERT INTO `inmoti6_mysite`.`comments` (`id`, `name`, `email`, `website`,
-//        `comment`, `timestamp`, `articleid`) VALUES (NULL, '$users_name',
-//        '$users_email', '$users_website', '$users_comment',
-//        CURRENT_TIMESTAMP, '$articleid');";
-
-
-    $sql = "INSERT INTO production ('well', 'production_date','oil','gas','water','gor','bsw','bean','thp','bhp')
-VALUES ('$well' ,'$production_date','$oil' ,'$gas','$water','$gor', '$bsw' , '$bean', '$thp', '$bhp')";
-
-    $result = mysqli_query($conn, $sql) or die ("");
-
-// if successfully updated.
-    if ($result) {
-        header("location: ../view_production.php");
-    } else {
-        header("location: ../edit_production.php");
-    }
+if ($conn->query($sql) === TRUE) {
+    header("location: ../view_production.php");
 }
-?>
+else{
+    header("location: ../add_production.php");
+}
+
+$conn->close();
+
+
+
+
+
 
