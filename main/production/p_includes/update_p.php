8e010c865d2748ce4016484ec19efdc2aa5c246c
Obed Kraine <prinsobed@gmail.com>
2016-08-21 03:11:13 +0100

    WELL selected and date selected + Update Updated, Still

--- main/production/p_includes/update_p.php
+++ main/production/p_includes/update_p.php
@@ -1,38 +1,53 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Obed Kraine, RGU-1314863 , o.k.boachie@rgu.ac.uk
- * Project: ProdPredict V1
- * Date: 8/1/2016
- * Time: 8:02 AM
- */
-
-// Setting up Connection with Database
-$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
-$username="bed8c15b456030"; // Mysql username
-$password="58380471"; // Mysql password
-$dbname="db_prodpredict"; // Database name
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-/**********************************************/
-
-$sql = "UPDATE production SET oil='".$_POST['oil_prod']."', gas='".$_POST['gas_prod']."', water='".$_POST['water_prod']."', gor='".$_POST['gas_oil_ratio']."', bsw='".$_POST['basic_sed_water']."', bean='".$_POST['bean']."', thp='".$_POST['tubing_hang_press']."', bhp='".$_POST['bottom_hole_pressure']."' WHERE well ='".$_POST['prod_well']."'  AND production_date = '".$_POST['prod_date']."'";
-$result=mysqli_query($conn, $sql) or die ("");
+echo 'Bonjour ' . htmlspecialchars($_POST["oil_prod"]) . '!';
+?>
+<!--.$_POST['oil_prod'];-->
+<!--.$_POST['gas_prod'];-->
+<!--.$_POST['water_prod'];-->
+<!--.$_POST['gas_oil_ratio'];-->
+<!--.$_POST['basic_sed_water'];-->
+<!--.$_POST['bean'];-->
+<!--.$_POST['tubing_hang_press'];-->
+<!--.$_POST['bottom_hole_pressure'];-->
+<!--.$_POST['prod_well'];-->
+<!--.$_POST['prod_date'];-->
 
 
-// if successfully updated.
-if($result){
-    header("location: ../view_production.php");
-}
-else {
-    header("location: ../edit_production.php");
-}
+
+///**
+// * Created by PhpStorm.
+// * User: Obed Kraine, RGU-1314863 , o.k.boachie@rgu.ac.uk
+// * Project: ProdPredict V1
+// * Date: 8/1/2016
+// * Time: 8:02 AM
+// */
+//
+//// Setting up Connection with Database
+//$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
+//$username="bed8c15b456030"; // Mysql username
+//$password="58380471"; // Mysql password
+//$dbname="db_prodpredict"; // Database name
+//
+//// Create connection
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+///**********************************************/
+//
+//$sql = "UPDATE production SET oil='".$_POST['oil_prod']."', gas='".$_POST['gas_prod']."', water='".$_POST['water_prod']."', gor='".$_POST['gas_oil_ratio']."', bsw='".$_POST['basic_sed_water']."', bean='".$_POST['bean']."', thp='".$_POST['tubing_hang_press']."', bhp='".$_POST['bottom_hole_pressure']."' WHERE well ='".$_POST['prod_well']."'  AND production_date = '".$_POST['prod_date']."'";
+//$result=mysqli_query($conn, $sql) or die ("");
+//
+//
+//// if successfully updated.
+//if($result){
+//    header("location: ../view_production.php");
+//}
+//else {
+//    header("location: ../edit_production.php");
+//}
 
 ?>
 
