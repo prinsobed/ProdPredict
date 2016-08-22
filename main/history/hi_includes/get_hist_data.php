104c348d2edab9fd7e19e521966fe23f17c13483
Obed Kraine <prinsobed@gmail.com>
2016-08-22 03:13:08 +0100

    History

--- main/history/hi_includes/get_hist_data.php
+++ main/history/hi_includes/get_hist_data.php
@@ -35,7 +35,8 @@ $h_hydro = $_POST['hydrocarbon'];
 $rep_type = $_POST['report_type'];
 
 // Form the SQL query that returns data values
-$strQuery = 'SELECT * FROM  production WHERE well = '.$h_well.' AND production_date BETWEEN "'.$h_start.'"AND"'.$h_end;
+//$strQuery = 'SELECT * FROM  production WHERE well = '.$h_well.' AND production_date BETWEEN "'.$h_start.'"AND"'.$h_end;
+$strQuery = "SELECT * FROM  production WHERE well = '$h_well' AND production_date BETWEEN '$h_start' AND '$h_end' ORDER BY production_date ASC";
 
 //  BETWEEN '#$h_start#' AND '#$h_end#' ORDER BY production_date ASC";
 
