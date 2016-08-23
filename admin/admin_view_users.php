8c4de9db77fd95e365c45d29a2b7e6215e1297a9
Obed Kraine <prinsobed@gmail.com>
2016-08-23 20:50:50 +0100

    theCount

--- admin/admin_view_users.php
+++ admin/admin_view_users.php
@@ -7,6 +7,18 @@
  * Time: 8:02 AM
  */
 
+$servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
+$username="bed8c15b456030"; // Mysql username
+$password="58380471"; // Mysql password
+$dbname="db_prodpredict"; // Database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 session_start();
 if(!$_SESSION['username']){
     header("Location: ../index.php");
@@ -183,17 +195,7 @@ if(!$_SESSION['username']){
                                         <?php
                                         include "../includes/ajax.php";
 
-                                        $servername="ap-cdbr-azure-east-c.cloudapp.net"; // Host name
-                                        $username="bed8c15b456030"; // Mysql username
-                                        $password="58380471"; // Mysql password
-                                        $dbname="db_prodpredict"; // Database name
 
-                                        // Create connection
-                                        $conn = new mysqli($servername, $username, $password, $dbname);
-                                        // Check connection
-                                        if ($conn->connect_error) {
-                                            die("Connection failed: " . $conn->connect_error);
-                                        }
 
 
                                         $sql = "SELECT id, firstname, lastname, company, email, password, user_type FROM users";
