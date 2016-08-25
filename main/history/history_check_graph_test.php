835d6fdb61b473bc72b49eabf48ee511fed7205b
Obed Kraine <prinsobed@gmail.com>
2016-08-25 02:10:06 +0100

    Prod Import again

--- main/history/history_check_graph_test.php
+++ main/history/history_check_graph_test.php
@@ -252,8 +252,8 @@ Chart Plotting Javascript
         bindto: '#chart',
         data: {
             columns: [
-                ['data1', 30, 200, 100, 400, 150, 250],
-                ['data2', 50, 20, 10, 40, 15, 25]
+                ['data1', 30, 200, 100, 400, 150, 250]
+//                ['data2', 50, 20, 10, 40, 15, 25]
             ]
         }
     });
