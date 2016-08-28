0b6755f607460bed96a278829c938d969b30bea7
Obed Kraine <prinsobed@gmail.com>
2016-08-28 23:02:40 +0100

    getting chart too work

--- main/production/import_production.php
+++ main/production/import_production.php
@@ -175,7 +175,7 @@ if ($conn->connect_error) {
 
                                         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                                             $dateFormatted = new DateTime($data[1]);
-                                            $import="INSERT into production(well, production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]', $dateFormatted,$data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],11)";
+                                            $import="INSERT into production(well, production_date, oil, gas, water, gor, bsw, bean, thp, bhp, api, ent_user) values('$data[0]', '28/08/2016', $data[2],$data[3],$data[4],$data[5],$data[6],$data[7],$data[8],$data[9],$data[10],11)";
                                             mysqli_query($conn, $import) or die(mysqli_error($conn));
                                         }
 
