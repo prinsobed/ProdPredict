8dc8691994e8d99c0fe7beb263cbba31d3740936
prinsobed <prinsobed@gmail.com>
2016-09-04 10:04:58 +0100

    LogOut

--- main/home.php
+++ main/home.php
@@ -53,7 +53,7 @@ if(!$_SESSION['username']){
                         <ul class="nav navbar-nav navbar-right">
                             <li><a href="#">Welcome<?php echo ", "."{$_SESSION['firstname']}"; ?></a></li>
                             <li><a href="#">Settings</a></li>
-                            <li><button type="button" class="btn navbar-btn btn-circle"><a href="../includes/logout.php">Log Out</a></button></li>
+                            <li><button type="button" class="btn navbar-btn btn-circle" formaction="<?php header('Location: ../includes/logout.php');?>">Log Out</a></button></li>
                         </ul>
                     </div>
                 </div>
